8d7688bcf6e3847bec55508f56512eca20914085
Joao <farias.joaovictor@gmail.com>
2022-10-14 13:43:14 -0300

    Instagram followers working

--- code.py
+++ code.py
@@ -1,16 +1,22 @@
 #Import mandatory libraries 
 import pandas as pd
 import requests
+from bs4 import BeautifulSoup
 
-#Read website list, get website name and try to find its instagram page
+r = requests.get('https://httpbin.org/basic-auth/user/pass', auth=(('user', 'pass')))
+
+#Read website list, get website name, and rename column label 
 data = pd.read_csv('vtex.txt', sep=' ', header=None, names=["Site"])
-page = data['Site'].str.split('.', expand=True)
-instagram_page = 'https://www.instagram.com/' + page[0]
+pages = data['Site'].str.split('.', expand=True)
+pages = pages.rename({0:'Nome do site'}, axis=1)
 
-#Requesting number of followers with requests package python
-instagram_request = requests.get(instagram_page).text
-start = '"edge_followed_by":{"count":'
-end = '}"'
-followers = instagram_request[instagram_request.find(start)+len(start):instagram_request.rfind(end)]
-
-print(followers)
+#Number of instagram followers
+for page in pages['Nome do site']:
+    page_instagram = 'https://www.instagram.com/' + page
+    print(page_instagram)
+    r = requests.get(page_instagram)
+    soup = BeautifulSoup(r.content)
+    followers = soup.find('meta', {'name': 'description'})['content']
+    follower_count = followers.split('Followers')[0]
+    print(follower_count)
+    break
