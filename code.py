6fd0d514ed41706cf46dcd2cb0d3577483ca8a90
Joao <farias.joaovictor@gmail.com>
2022-11-08 16:06:39 -0300

    first viable edition

--- code.py
+++ code.py
@@ -1,6 +1,7 @@
 #Import mandatory libraries 
 import pandas as pd
 import requests
+import csv
 from bs4 import BeautifulSoup
 
 r = requests.get('https://httpbin.org/basic-auth/user/pass', auth=(('user', 'pass')))
@@ -10,13 +11,26 @@ data = pd.read_csv('vtex.txt', sep=' ', header=None, names=["Site"])
 pages = data['Site'].str.split('.', expand=True)
 pages = pages.rename({0:'Nome do site'}, axis=1)
 
-#Number of instagram followers
+f = open("modelo2.csv", "w")
+writer = csv.DictWriter(f, fieldnames=["Site", "Cliente", "Instagram", "Seguidores"])
+
+#Number of instagram followers + exporting to csv
+f = open("modelo2.csv", "w")
+writer = csv.DictWriter(f, fieldnames=["Site", "Cliente", "Instagram", "Seguidores"])
+writer.writeheader()
+f = open("modelo2.csv", "w")
+writer = csv.DictWriter(f, fieldnames=["Site", "Cliente", "Instagram", "Seguidores"])
+writer.writeheader()
 for page in pages['Nome do site']:
     page_instagram = 'https://www.instagram.com/' + page
-    print(page_instagram)
     r = requests.get(page_instagram)
-    soup = BeautifulSoup(r.content)
-    followers = soup.find('meta', {'name': 'description'})['content']
-    follower_count = followers.split('Followers')[0]
-    print(follower_count)
-    break
+    soup = BeautifulSoup(r.content, features="html.parser")
+    try:
+        followers = soup.find('meta', {'name': 'description'})['content']
+        follower_count = followers.split('Followers')[0]
+        follower_count = follower_count.replace('K', '000').replace('M', '000000')
+        if int(follower_count) > 20000:
+            writer.writerow({"Site": page + '.com.br' , "Cliente": page, "Instagram": page_instagram, "Seguidores": follower_count})
+    except: 
+        pass
+f.close()   
