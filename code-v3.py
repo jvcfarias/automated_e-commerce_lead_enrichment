b4ea0a11089368cd447932dd02bb311793d54b9d
Diego de Freitas Kato <katodiego@gmail.com>
2022-11-14 09:36:50 -0300

    modelo3.csv

--- code-v3.py
+++ code-v3.py
@@ -4,6 +4,7 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 from datetime import datetime
+import time
 
 error_domain = "ERROR_DOMAIN"
 error_instagram = "ERROR_INSTAGRAM"
@@ -22,6 +23,7 @@ def get_instagram_account(site):
     elements = soup.select('[href*="instagram.com/"]')
     if elements:
       instagram_page = elements[0]['href']
+      print(instagram_page)
       instagram_account = instagram_page.split('instagram.com/')[1].split('/', 1)[0]
       return instagram_account
 
@@ -33,31 +35,78 @@ def get_instagram_account(site):
 
 def get_follower_count(instagram_account):
   try:
+    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36"}
+    #headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36"}
+    r = requests.get('https://www.instagram.com/' + instagram_account, headers=headers, timeout=5)
+
+    if r.status_code == 429:
+      return -1
+
+    if r.status_code != 200:
+      return -1
+
+    soup = BeautifulSoup(r.content, features="html.parser")
+    followers = soup.find('meta', {'name': 'description'})['content']
+    follower_count = followers.split('Followers')[0]
+    follower_count = follower_count.replace('K', '000').replace('M', '000000').replace(',','').replace('.','')
+    return int(follower_count)
+
+  except: 
+    return -1
+
+
+def get_follower_count2(instagram_account):
+  try:
     url = 'https://i.instagram.com/api/v1/users/web_profile_info/?username=' + instagram_account
     
     headers = {"User-Agent": "Instagram 76.0.0.15.395 Android (24/7.0; 640dpi; 1440x2560; samsung; SM-G930F; herolte; samsungexynos8890; en_US; 138226743)"}
     r = requests.get(url, headers=headers, timeout=5)
-    if r.status_code != 200:
+    if r.status_code != 200: 
       return -1
-      
+
+    # f = open(instagram_account + ".html", "w")
+    # f.write(r.content)
+    # f.close
+    
     json_data = r.json()
     return json_data["data"]["user"]["edge_followed_by"]["count"]
   except: 
     return -1
 
 
+def get_last_row_number():
+  try: 
+    with open("modelo3.csv", 'r') as fread:
+      for count, line in enumerate(fread):
+          pass
+    fread.close();
+
+    print('last_row', count)
+    
+    return count
+  except:
+    return 0
+
+
 #Read website list, get website name, and rename column label 
 data = pd.read_csv('vtex.txt', sep=' ', header=None, names=["Site"])
 sites = data['Site'];
 
-f = open("modelo3.csv", "w")
-writer = csv.DictWriter(f, fieldnames=["Site", "Instagram", "Seguidores", "Data"])
-writer.writeheader()
+last_row = get_last_row_number()
 
-count = 0
+fieldnames = ["Site", "Instagram", "Seguidores", "Data"]
+f = open("modelo3.csv", "a")
+writer = csv.DictWriter(f, fieldnames=fieldnames, lineterminator = '\n')
+
+followers_errors_count = 0
+row = 0
 for site in sites:
-  count += 1
-  print(str(count) + ' - ' + site);
+  row += 1
+
+  if row <= last_row:
+    continue
+
+  print(str(row) + ' - ' + site);
 
   instagram_account = get_instagram_account(site)
   
@@ -66,14 +115,31 @@ for site in sites:
     instagram_account = get_instagram_account("www." + site);
 
   if "ERROR" in instagram_account:
-    print(str(count) +' - ' + site + ' - ' + instagram_account + ' - ' + '-1' + ' - ' + datetime.now().strftime("%H:%M:%S"));
+    print(str(row) +' - ' + site + ' - ' + instagram_account + ' - ' + '-1' + ' - ' + datetime.now().strftime("%H:%M:%S"));
     writer.writerow({"Site": site , "Instagram": instagram_account, "Seguidores": -1, "Data": datetime.now().strftime("%H:%M:%S")})
     continue
   
-  follower_count = get_follower_count(instagram_account)
+  follower_count = get_follower_count2(instagram_account)
+
+  # verifica se o instagram bloqueou
+  if follower_count == -1:
+    followers_errors_count += 1
+  else:
+    followers_errors_count = 0
+
+  if followers_errors_count > 2:
+    f.close()
+    exit()
+  # --
 
   #print(f'{count} - {site} - {instagram_account} - {follower_count} - {datetime.now().strftime("%H:%M:%S")}');
-  print(str(count) +' - ' + site + ' - ' + instagram_account + ' - ' + str(follower_count) + ' - ' + datetime.now().strftime("%H:%M:%S"));
+  print(str(row) +' - ' + site + ' - ' + instagram_account + ' - ' + str(follower_count) + ' - ' + datetime.now().strftime("%H:%M:%S"))
   writer.writerow({"Site": site , "Instagram": instagram_account, "Seguidores": follower_count, "Data": datetime.now().strftime("%H:%M:%S")})
+
+  f.close()
+  f = open("modelo3.csv", "a")
+  writer = csv.DictWriter(f, fieldnames=fieldnames, lineterminator = '\n')
+
+  time.sleep(15)
   
 f.close()   
